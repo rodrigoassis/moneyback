de75c4f9f0c7182b50087ed53b04a343ef1b3926
Júlio Bueno <julioce@gmail.com>
2011-12-21 15:05:50 -0200

    Validação mais inteligente

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -2,5 +2,6 @@
 
 class Situation < ActiveRecord::Base
   validates_presence_of :renda, :custo, :total_devedor, :fatura, :juros, :minimo_fatura, :message => "não pode estar em branco"
-  validates_numericality_of :renda, :custo, :total_devedor, :fatura, :juros, :minimo_fatura, :message => "devem ser números"
+  
+  validates_numericality_of :renda, :custo, :total_devedor, :fatura, :juros, :minimo_fatura, :allow_nil => true, :message => "deve ser um valor monetário"
 end
